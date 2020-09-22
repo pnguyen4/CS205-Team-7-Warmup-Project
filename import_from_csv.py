e78729b7c298b329c17afb22eac3207555c558a6
Phillip Nguyen <pnguyen4711@gmail.com>
2020-09-22 17:59:34 -0400

    Add python wrappers for 2 queries & organize code

--- import_from_csv.py
+++ import_from_csv.py
@@ -1,25 +1,35 @@
-import sqlite3, csv
+import sqlite3, csv, os
 
-conn = sqlite3.connect('StateUniversities.db')
-c = conn.cursor()
+def create_database():
+    conn = sqlite3.connect('StateUniversities.db')
+    c = conn.cursor()
 
-c.execute('''CREATE TABLE states (id integer PRIMARY KEY, name text, capital text, population integer, numberOfInstitutions integer)''')
-file1 = open('states.csv', 'r')
+    c.execute('''CREATE TABLE states (id integer PRIMARY KEY, name text, capital text, population integer, numberOfInstitutions integer)''')
+    file1 = open('states.csv', 'r')
 
-rows = csv.reader(file1, delimiter=',', quotechar='"')
-for row in rows:
-    c.execute('''INSERT INTO states(name, capital, population, numberofInstitutions) VALUES (?,?,?,?)''', row)
-    conn.commit()
-file1.close()
+    rows = csv.reader(file1, delimiter=',', quotechar='"')
+    for row in rows:
+        c.execute('''INSERT INTO states(name, capital, population, numberofInstitutions) VALUES (?,?,?,?)''', row)
+        conn.commit()
+    file1.close()
 
-c.execute('''CREATE TABLE universities (id integer PRIMARY KEY, name text, population integer, city text, state integer,FOREIGN KEY (state) REFERENCES states (id) )''')
+    c.execute('''CREATE TABLE universities (id integer PRIMARY KEY, name text, population integer, city text, state integer,FOREIGN KEY (state) REFERENCES states (id) )''')
 
-file2 = open('universities.csv', 'r')
-rows = csv.reader(file2, delimiter=',', quotechar='"')
-for row in rows:
-    c.execute('''INSERT INTO universities(name, population, city, state) VALUES(?,?,?, (SELECT id FROM states WHERE name = ?))''', row)
-    conn.commit()
-flie2.close()
+    file2 = open('universities.csv', 'r')
+    rows = csv.reader(file2, delimiter=',', quotechar='"')
+    for row in rows:
+        c.execute('''INSERT INTO universities(name, population, city, state) VALUES(?,?,?, (SELECT id FROM states WHERE name = ?))''', row)
+        conn.commit()
+    file2.close()
 
-conn.close()
-print("\n imported successfully")
+    print("\n imported successfully")
+    return conn
+
+def load_database():
+    if os.path.exists('StateUniversities.db'):
+        conn = sqlite3.connect('StateUniversities.db')
+        print("loading existing database")
+    else:
+        conn = create_database()
+        print("created new database")
+    return conn
--- loading_database.py
+++ loading_database.py
@@ -1,13 +1,66 @@
 import sqlite3, csv
 from io import StringIO
 
-# Read database to tempfile
-con = sqlite3.connect('StateUniversities.db')
-c = conn.cursor()
+import import_from_csv as db
 
-tempfile = StringIO()
-for line in con.iterdump():
-    tempfile.write('%s\n' % line)
-    
-con.close()
-tempfile.seek(0)
+# new test for loading data from database
+def test():
+    conn = db.load_database()
+    print("Running test cases")
+    # Tests for example 1 and 2
+    assert get_num_institutions_state(conn, "Vermont") == 32
+    assert get_state_university(conn, "Cornell University") == "New York"
+    print("Passed all test cases")
+    conn.close()
+
+# Example 1: query one value from one row
+def get_num_institutions_state(conn, arg):
+    c = conn.cursor()
+    # arg must be in a tuple, or else each letter is interpreted as an arg
+    c.execute('''SELECT numberOfInstitutions FROM states WHERE name=?''', (arg,))
+    # execute() returns a list of tuples but we know that this query should
+    # only return one row with only one value. Not true for all queries!
+    return c.fetchall()[0][0]
+
+# Example 2: query one value that depends on both rows
+def get_state_university(conn, arg):
+    c = conn.cursor()
+    # universities.state is the foreign key linking to
+    # states.id, the row index and primary key in states table
+    c.execute('''SELECT states.name
+              FROM (universities JOIN states ON universities.state = states.id)
+              WHERE universities.name = ?''', (arg,))
+    return c.fetchall()[0][0]
+
+def get_population_university(conn, arg):
+    raise NotImplementedError
+
+def get_city_university(conn, arg):
+    raise NotImplementedError
+
+def get_population_state(conn, arg):
+    raise NotImplementedError
+
+def get_capital_state(conn, arg):
+    raise NotImplementedError
+
+def list_university_state(conn, arg):
+    raise NotImplementedError
+
+def in_state_capital_university(conn, arg):
+    raise NotImplementedError
+
+test()
+
+# old test for loading data from database
+#########################
+# Read database to tempfile
+# con = sqlite3.connect('StateUniversities.db')
+# c = con.cursor()
+#
+# tempfile = StringIO()
+# for line in con.iterdump():
+#     tempfile.write('%s\n' % line)
+#
+# con.close()
+# tempfile.seek(0)
